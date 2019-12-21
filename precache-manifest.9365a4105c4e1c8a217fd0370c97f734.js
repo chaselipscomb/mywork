9458da7d0cfcdac40fe040894d128399bc602215
Chase Lipscomb <chaselipscomb@Chases-MacBook-Pro.local>
2019-12-20 22:08:38 -0800

    Updates

---
+++ precache-manifest.9365a4105c4e1c8a217fd0370c97f734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "832fdd6dc5b738b70bdcc69fcfc7dd98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97f1adfa9e616593ad98",
+    "url": "/static/css/main.f4645d51.chunk.css"
+  },
+  {
+    "revision": "96b8d017baba007be6a1",
+    "url": "/static/js/2.96756778.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.96756778.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97f1adfa9e616593ad98",
+    "url": "/static/js/main.f61d945e.chunk.js"
+  },
+  {
+    "revision": "efc93394c52cf1132811",
+    "url": "/static/js/runtime-main.9eb0259d.js"
+  }
+]);
