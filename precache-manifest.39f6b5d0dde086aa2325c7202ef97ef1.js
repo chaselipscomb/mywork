4cb703b07aa833e944f3d537d0fc5c2bd4414a33
Chase Lipscomb <chaselipscomb@Chases-MacBook-Pro.local>
2019-12-23 12:35:57 -0800

    Updates

--- precache-manifest.39f6b5d0dde086aa2325c7202ef97ef1.js
+++ precache-manifest.39f6b5d0dde086aa2325c7202ef97ef1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "09e431cf66a3db30e3f890b808729cf0",
+    "revision": "349d2a3811ea00967d9b1c224fd84559",
     "url": "/mywork/index.html"
   },
   {
-    "revision": "df9d71be1c270a751f87",
+    "revision": "c0d23211e4e4013256ee",
     "url": "/mywork/static/css/main.f4645d51.chunk.css"
   },
   {
-    "revision": "c854a324657e3dc0e92c",
-    "url": "/mywork/static/js/2.3a5b8d82.chunk.js"
+    "revision": "3a8f12a6f425a0a025d4",
+    "url": "/mywork/static/js/2.71366d49.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/mywork/static/js/2.3a5b8d82.chunk.js.LICENSE"
+    "url": "/mywork/static/js/2.71366d49.chunk.js.LICENSE"
   },
   {
-    "revision": "df9d71be1c270a751f87",
-    "url": "/mywork/static/js/main.a84a3fd1.chunk.js"
+    "revision": "c0d23211e4e4013256ee",
+    "url": "/mywork/static/js/main.8134bcab.chunk.js"
   },
   {
     "revision": "1a13be35083a0d0a64c7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mywork/precache-manifest.fe82be9b8c2a5e2e5fae156624a7f426.js"
+  "/mywork/precache-manifest.39f6b5d0dde086aa2325c7202ef97ef1.js"
 );
 
 self.addEventListener('message', (event) => {
