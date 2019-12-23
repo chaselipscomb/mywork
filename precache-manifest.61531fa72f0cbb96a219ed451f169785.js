9a31e4be3441757280cb481a8ed21471ea37ed0f
Chase Lipscomb <chaselipscomb@Chases-MacBook-Pro.local>
2019-12-23 12:47:39 -0800

    Updates

--- precache-manifest.61531fa72f0cbb96a219ed451f169785.js
+++ precache-manifest.61531fa72f0cbb96a219ed451f169785.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "349d2a3811ea00967d9b1c224fd84559",
+    "revision": "27a3225b9ea53ff6e0f71bc5693702d1",
     "url": "/mywork/index.html"
   },
   {
-    "revision": "c0d23211e4e4013256ee",
+    "revision": "ca45f20b55a11406305b",
     "url": "/mywork/static/css/main.f4645d51.chunk.css"
   },
   {
-    "revision": "3a8f12a6f425a0a025d4",
-    "url": "/mywork/static/js/2.71366d49.chunk.js"
+    "revision": "c854a324657e3dc0e92c",
+    "url": "/mywork/static/js/2.3a5b8d82.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/mywork/static/js/2.71366d49.chunk.js.LICENSE"
+    "url": "/mywork/static/js/2.3a5b8d82.chunk.js.LICENSE"
   },
   {
-    "revision": "c0d23211e4e4013256ee",
-    "url": "/mywork/static/js/main.8134bcab.chunk.js"
+    "revision": "ca45f20b55a11406305b",
+    "url": "/mywork/static/js/main.4466c5bc.chunk.js"
   },
   {
     "revision": "1a13be35083a0d0a64c7",
