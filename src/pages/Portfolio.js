088e87b7f0eff28788ecdf634f5e8383cf5f3580
Chase Lipscomb <chaselipscomb@Chases-MacBook-Pro.local>
2020-01-15 22:02:30 -0800

    updated portfolio page and some text sizes

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -57,16 +57,16 @@ const Portfolio = () => {
         <>
             <div className='portfoliocontainer'>
                 <center><h1 style={styles.h1}>Portfolio</h1></center>
-                <a href={work[0].link} ><img className='img1' src={work[0].image} alt={work[0].name} onMouseOver={(event) => setInfo1(event.target.alt)} /></a>
-                <a href={work[1].link}><img className='img1' src={work[1].image} alt={work[1].name} onMouseOver={(event) => setInfo1(event.target.alt)} /></a>
+                <div className='img1'><a href={work[0].link} ><img className="workimage" src={work[0].image} alt={work[0].name} onMouseOver={(event) => setInfo1(event.target.alt)} /></a></div>
+                <div className='img1'><a href={work[1].link}><img className="workimage" src={work[1].image} alt={work[1].name} onMouseOver={(event) => setInfo1(event.target.alt)} /></a></div>
                 <div className='info'><center><h3 className='infos' ><b>{info1}</b></h3></center></div>
-                <a href={work[2].link}><img className='img1' src={work[2].image} alt={work[2].name} onMouseOver={(event) => setInfo2(event.target.alt)} /></a>
-                <a href={work[3].link}><img className='img1' src={work[3].image} alt={work[3].name} onMouseOver={(event) => setInfo2(event.target.alt)} /></a>
+                <div className="img1"><a href={work[2].link}><img className='workimage' src={work[2].image} alt={work[2].name} onMouseOver={(event) => setInfo2(event.target.alt)} /></a></div>
+                <div className="img1"><a href={work[3].link}><img className='workimage' src={work[3].image} alt={work[3].name} onMouseOver={(event) => setInfo2(event.target.alt)} /></a></div>
                 <div className='info'><center><h3 className='infos' ><b>{info2}</b></h3></center></div>
-                <a href={work[4].link}><img className='img1' src={work[4].image} alt={work[4].name} onMouseOver={(event) => setInfo3(event.target.alt)} /></a>
-                <a href={work[5].link}><img className='img1' src={work[5].image} alt={work[5].name} onMouseOver={(event) => setInfo3(event.target.alt)} /></a>
+                <div className="img1"><a href={work[4].link}><img className='workimage' src={work[4].image} alt={work[4].name} onMouseOver={(event) => setInfo3(event.target.alt)} /></a></div>
+                <div className="img1"><a href={work[5].link}><img className='workimage' src={work[5].image} alt={work[5].name} onMouseOver={(event) => setInfo3(event.target.alt)} /></a></div>
                 <div className='info'><center><h3 className='infos' ><b>{info3}</b></h3></center></div>
-                <center><a href={work[6].link} ><img className='img1' src={work[6].image} alt={work[6].name} onMouseOver={(event) => setInfo4(event.target.alt)} /></a></center>
+                <center><div className="img1"><a href={work[6].link} ><img className='workimage' src={work[6].image} alt={work[6].name} onMouseOver={(event) => setInfo4(event.target.alt)} /></a></div></center>
                 <div className='info'><center><h3 className='infos' ><b>{info4}</b></h3></center></div>
                 
             </div>
